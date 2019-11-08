3b829908b74d06f092489d0d3240ca1df20daf38
AidenBow <AidenBow@users.noreply.github.com>
2019-11-07 16:51:12 -0800

    validate user id

--- users/userRouter.js
+++ users/userRouter.js
@@ -9,7 +9,7 @@ router.post('/', (req, res) => {
       res.status(200).json(user)
     })
     .catch(err => {
-      res.status(500).json(err)
+      res.status(500).json({message: "500 server error"})
     })
 });
 
@@ -23,18 +23,12 @@ router.get('/', (req, res) => {
       res.status(200).json(users)
     })
     .catch(err => {
-      res.status(500).json(err)
+      res.status(500).json({message: "500 server error"})
     })
 });
 
-router.get('/:id', (req, res) => {
-  userDb.getById(req.params.id)
-    .then(user => {
-      res.status(200).json(user)
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    })
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user)
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -43,12 +37,18 @@ router.get('/:id/posts', (req, res) => {
       res.status(200).json(posts)
     })
     .catch(err => {
-      res.status(500).json(err)
+      res.status(500).json({message: "500 server error"})
     })
 });
 
 router.delete('/:id', (req, res) => {
-
+  userDb.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({message: `user ${req.params.id} has been deleted`})
+    })
+    .catch(err => {
+      res.status(500).json({message: "500 server error"})
+    })
 });
 
 router.put('/:id', (req, res) => {
@@ -58,7 +58,15 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+  userDb.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({message: `user id:${req.params.id} id not valid`})
+      }
+    })
 };
 
 function validateUser(req, res, next) {
